3ce0659b628ddaedb54e40e227cb6434cfd6ca32
Rohit Agrawal <110839621+rohitagr0310@users.noreply.github.com>
2023-08-09 21:07:51 +0530

    header component started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg'
-import './App.css'
+/* eslint-disable require-jsdoc */
+import React from "react";
+import Header from "./components/header/Header.js";
+// Other imports and code
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* The rest of your app */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
