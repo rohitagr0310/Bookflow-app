b22ab0acc26a7b3069440962418d0639ea1fca8c
qaidjoharj53 <qaidjoharytqt@gmail.com>
2023-08-11 21:38:05 +0530

    Added Login Page Components

--- src/components/login_page/LoginPage.js
+++ src/components/login_page/LoginPage.js
@@ -4,62 +4,87 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh"
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    gap: theme.spacing(2)
-  }
+	root: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		height: "100vh",
+	},
+	form: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		gap: theme.spacing(2),
+	},
+	p: {
+		cursor: "pointer",
+		color: theme.palette.primary.main,
+		textDecoration: "underline",
+		marginTop: theme.spacing(2),
+		fontSize: "14px",
+		fontWeight: "bold",
+	},
 }));
 
 const LoginPage = () => {
-  const classes = useStyles();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+	const classes = useStyles();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [isSignUp, setIsSignUp] = useState(false);
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
 
-  const handleLogin = () => {
-    // Perform login logic here
-    console.log("Username:", username);
-    console.log("Password:", password);
-  };
+	const handleSignIn = () => {
+		// Perform sign-in logic here
+		console.log("Email:", email);
+		console.log("Password:", password);
+	};
 
-  return (
-    <div className={classes.root}>
-      <form className={classes.form}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <Button variant="contained" color="primary" onClick={handleLogin}>
-          Login
-        </Button>
-      </form>
-    </div>
-  );
+	const handleSignUp = () => {
+		// Perform sign-up logic here
+		console.log("Email:", email);
+		console.log("Password:", password);
+	};
+
+	return (
+		<div className={classes.root}>
+			<form className={classes.form}>
+				<TextField
+					label="Email"
+					placeholder="Email"
+					variant="outlined"
+					value={email}
+					onChange={handleEmailChange}
+				/>
+				<TextField
+					label="Password"
+					type="password"
+					placeholder="Password"
+					variant="outlined"
+					value={password}
+					onChange={handlePasswordChange}
+				/>
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={isSignUp ? handleSignUp : handleSignIn}>
+					{isSignUp ? "Sign Up" : "Sign In"}
+				</Button>
+				<p className={classes.p} onClick={() => setIsSignUp(!isSignUp)}>
+					{isSignUp
+						? "Already have an account? Sign In"
+						: "Don't have an account? Sign Up"}
+				</p>
+			</form>
+		</div>
+	);
 };
 
 export default LoginPage;
