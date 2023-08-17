df73dc274505b41fb8407f2c24f169a8390b2d7b
jain-anshika <115166105+jain-anshika@users.noreply.github.com>
2023-08-17 21:57:11 +0530

    I created side bar and header in admin panel.

---
+++ src/components/admin_panel/pages/AddBook.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddBook = () => {
+  return <div>Add Book Page</div>;
+};
+
+export default AddBook;
---
+++ src/components/admin_panel/pages/ManageStudents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageStudents = () => {
+  return <div>Manage Students Page</div>;
+};
+
+export default ManageStudents;
