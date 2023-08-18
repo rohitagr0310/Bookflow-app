e5fa7012ecef03ad692909dc98951e05c4748ec4
jain-anshika <115166105+jain-anshika@users.noreply.github.com>
2023-08-18 21:19:48 +0530

    error in connecting pages wih nav bar

--- src/components/admin_panel/AdminNav.js
+++ src/components/admin_panel/AdminNav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./AdminNavStyle.css";
-import "./pages/Dashboard";
 
 const Navigation = () => {
   const navigationStyle = {
