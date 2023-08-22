bfd3da3259b732fb35aa0eaaa5f80670cf8f4595
qaidjoharj53 <qaidjoharytqt@gmail.com>
2023-08-22 09:56:26 +0530

    Folder/Files Hierarchy updated

--- src/pages/home_page/HomePage.jsx
+++ src/pages/home_page/HomePage.jsx
@@ -5,7 +5,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Header from "./header/Header";
-import libraryImage from "./Images/library.jpg";
+import libraryImage from "../../Images/library.jpg";
 import "./_HomePage.css";
 
 const theme = createTheme();
@@ -17,41 +17,34 @@ const HomePage = () => {
         <Header />
         <Container maxWidth="lg">
           <Typography variant="h3" className="mainHeading" gutterBottom>
-            Welcome to BookFlow Library
+            WebApp to BookFlow Library
           </Typography>
           <Typography variant="body1" className="contentText" paragraph>
-            This is a Library Management WebApp designed to serve the needs of our college{"'"}s library.
-            The app offers an intuitive platform
-            for both students and administrators to interact with the library{"'"}s resources.
+            This is a Library Management WebApp designed to serve the needs of our college{"'"}s library. The app offers
+            an intuitive platform for both students and administrators to interact with the library{"'"}s resources.
           </Typography>
           <section className="section">
             <Typography variant="h4" className="sectionHeading">
               About Us
             </Typography>
             <Typography variant="body1" className="sectionContent" paragraph>
-              Our Library Management WebApp simplifies the process of borrowing and returning books,
-              making the library experience seamless and efficient.
+              Our Library Management WebApp simplifies the process of borrowing and returning books, making the library
+              experience seamless and efficient.
             </Typography>
           </section>
           <section className="section">
             <Typography variant="h4" className="sectionHeading">
               Features
             </Typography>
+            <Typography className="sectionContent">• Easy browsing and searching of available books •</Typography>
             <Typography className="sectionContent">
-            • Easy browsing and searching of available books •
+              • Access to personalized student and administrator panels •
             </Typography>
             <Typography className="sectionContent">
-            • Access to personalized student and administrator panels •
-            </Typography>
-            <Typography className="sectionContent">
-            • Quick and convenient book borrowing and return process •
+              • Quick and convenient book borrowing and return process •
             </Typography>
           </section>
-          <img
-            src={libraryImage}
-            alt="Library"
-            className="libraryImage"
-          />
+          <img src={libraryImage} alt="Library" className="libraryImage" />
           <section className="section">
             <Typography variant="h4" className="sectionHeading">
               Testimonials
@@ -60,17 +53,19 @@ const HomePage = () => {
               <Card className="testimonialCard">
                 <CardContent>
                   <Typography variant="body1" className="testimonialContent">
-                    &#34;BookFlow has made managing library resources a breeze.
-                    It{"'"}s user-friendly and has transformed our library experience.&#34;
-                    <br /><br />~ Qaidjohar Jukker, Student
+                    &#34;BookFlow has made managing library resources a breeze. It{"'"}s user-friendly and has
+                    transformed our library experience.&#34;
+                    <br />
+                    <br />~ Qaidjohar Jukker, Student
                   </Typography>
                 </CardContent>
               </Card>
               <Card className="testimonialCard">
                 <CardContent>
                   <Typography variant="body1" className="testimonialContent">
-              &#34;As an administrator, BookFlow has improved our library operations tremendously.
-              It{"'"}s a must-have tool.&#34; <br /><br />~ Adarsh Regar, Administrator
+                    &#34;As an administrator, BookFlow has improved our library operations tremendously. It{"'"}s a
+                    must-have tool.&#34; <br />
+                    <br />~ Adarsh Regar, Administrator
                   </Typography>
                 </CardContent>
               </Card>
