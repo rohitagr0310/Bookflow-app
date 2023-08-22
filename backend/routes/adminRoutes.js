922e7160c55b5d748f24e676d4200a031138c427
Rohit Agrawal <rohitagr2610@gmail.com>
2023-08-23 00:13:47 +0530

    backend - adminpanel - allbooks

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-
-// Import adminController
 import adminController from "../controllers/adminController.js";
+
 const adminrouter = Router();
 
 // Define routes
 adminrouter.get("/admin", adminController.getAllAdmins);
-// Add more routes for admin actions
+
+adminrouter.post("/addBook", adminController.addBook);
+
+adminrouter.get("/all-books", adminController.getAllBooks);
 
 export default adminrouter;
