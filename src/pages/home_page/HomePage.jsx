6165c56d11a5e7e93b01335abbe84ac908e7aa9f
jayesh2474 <divyeshjoshi7474@gmail.com>
2023-08-27 18:59:23 +0530

    Testimonials

--- src/pages/home_page/HomePage.jsx
+++ src/pages/home_page/HomePage.jsx
@@ -11,6 +11,22 @@ import background4 from "../../images/background4.jpg"; // Import your images
 const theme = createTheme();
 
 const HomePage = () => {
+  const testimonials = [
+    {
+      name: "Jordy",
+      quote: "This website changed my life. I can't believe how amazing it is!"
+    },
+    {
+      name: "Ankit Jain",
+      quote:
+        "I've been using this website for years, and it never disappoints."
+    },
+    {
+      name: "Jayesh Ji Joshi",
+      quote:
+        "The content on this website is top-notch. I recommend it to everyone."
+    }
+  ];
   return (
     <ThemeProvider theme={theme}>
       <div className="root">
@@ -64,6 +80,18 @@ const HomePage = () => {
             }}
           >
             {/* Add content for Section 4 here */}
+          </section>
+          {/* Section for Testimonials */}
+          <section className="section testimonialCards">
+            <h2 className="sectionHeading">What Our Users Say</h2>
+            <div className="testimonialCardContainer">
+              {testimonials.map((testimonial, index) => (
+                <div className="testimonialCard" key={index}>
+                  <p className="testimonialContent">{testimonial.quote}</p>
+                  <p className="testimonialName">{testimonial.name}</p>
+                </div>
+              ))}
+            </div>
           </section>
         </Container>
       </div>
