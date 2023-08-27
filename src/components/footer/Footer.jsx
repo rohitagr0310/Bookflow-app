4e5e67c98546d946917605b159403a6c4dff88b2
Qaidjohar Jukker <113633954+qaidjoharj53@users.noreply.github.com>
2023-08-27 12:14:02 +0000

    Footer furnished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Typography, IconButton } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -10,55 +10,22 @@ import "./_Footer.css";
  * @returns {JSX.Element} Footer
  */
 function Footer () {
-  const [showAboutUsText, setShowAboutUsText] = useState(false);
-  const [showContactInfo, setShowContactInfo] = useState(false);
-
-  const toggleAboutUsText = () => {
-    setShowAboutUsText(!showAboutUsText);
-  };
-  const toggleContactInfo = () => {
-    setShowContactInfo(!showContactInfo);
-  };
   return (
     <footer className="footer">
       <Container maxWidth="lg">
         <div className="columnsContainer">
           <div className="column">
             <Typography variant="h6" className="columnTitle">
-              {/* Render "About Us" as a clickable element */}
-              <span onClick={toggleAboutUsText} className="aboutUsButton">
-                About Us
-              </span>
+              About Us
             </Typography>
-            {/* Render additional text conditionally */}
-            {showAboutUsText && (
-              <p className="smallText">
-                Our Library Management WebApp simplifies the process of
-                borrowing and returning books, making the library experience
-                seamless and efficient.
-              </p>
-            )}
-            <a onClick={toggleContactInfo} className="contactButton">
+            <a className="columnLink">
               Contact
             </a>
-            {showContactInfo && (
-              <div>
-                <p className="smallText">Contact Information:</p>
-                <p className="smallText">Email: contact@example.com</p>
-                <p className="smallText">Phone: +1 (123) 456-7890</p>
-              </div>
-            )}
-            <a href="http://localhost:3002/#" className="columnLink">
+            <a href="/" className="columnLink">
               Home
             </a>
             <a href="#" className="columnLink">
               Services
-            </a>
-            <a href="#" className="columnLink">
-              Blog
-            </a>
-            <a href="#" className="columnLink">
-              FAQ
             </a>
           </div>
           <div className="column">
@@ -77,26 +44,12 @@ function Footer () {
             <a href="#" className="columnLink">
               Security
             </a>
-            <a href="#" className="columnLink">
-              Sitemap
-            </a>
           </div>
           <div className="column">
-            {/* Subscribe column */}
-            <Typography variant="h6" className="columnTitle">
-              Subscribe
+            <Typography variant="h3" className="logo">
+              BookFlow
             </Typography>
-            {/* Add your subscribe input and button here */}
-            <div className="subscribeForm">
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="subscribeInput"
-              />
-              <button className="subscribeButton">Subscribe</button>
-            </div>
           </div>
-          <div className="column"></div>
         </div>
         <div className="socialIconsContainer">
           <IconButton
