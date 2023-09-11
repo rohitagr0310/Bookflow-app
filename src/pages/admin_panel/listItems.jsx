b52b2fa1ea13d087f6e444d10c83ba25898d1629
Rohit Agrawal <110839621+rohitagr0310@users.noreply.github.com>
2023-09-12 00:17:02 +0530

    firebase + link error fixed

--- src/pages/admin_panel/listItems.jsx
+++ src/pages/admin_panel/listItems.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react";
+import { Link } from "react-router-dom";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
