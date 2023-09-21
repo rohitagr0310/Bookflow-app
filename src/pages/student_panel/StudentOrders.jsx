0ae7555b7cfb811fc36045aad03f4ddc09192414
Rohit Agrawal <110839621+rohitagr0310@users.noreply.github.com>
2023-09-21 23:33:17 +0530

    footer and studentorder fxed

--- src/pages/student_panel/StudentOrders.jsx
+++ src/pages/student_panel/StudentOrders.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { FormControl, Grid, Select, MenuItem, Table, TableBody, TableCell, TableHead, TableRow, CardContent } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
 function BookTable () {
   // eslint-disable-next-line no-unused-vars
   const [books, setBooks] = useState([
     {
       id: 1,
-      Accession_Number: "Accession_Number1",
       name: "Book 1",
       stream: "stream A",
       submissionDate: "2023-09-10",
@@ -14,7 +13,6 @@ function BookTable () {
     },
     {
       id: 2,
-      Accession_Number: "Accession_Number2",
       name: "Book 2",
       stream: "stream B",
       submissionDate: "2023-09-08",
@@ -22,49 +20,32 @@ function BookTable () {
     }
     // Add more book data as needed
   ]);
-  const [selectedTable, setSelectedTable] = useState("test");
 
   return (
-    <card >
-      <CardContent>
-        <h1>Padding Book</h1>
-        <div>
-          <FormControl>
-            <Grid container spacing={1}>
-              <Select value={selectedTable} onChange={(e) => setSelectedTable(e.target.value)}>
-                <MenuItem value="test">test</MenuItem>
-                <MenuItem value="library">Library</MenuItem>
-                <MenuItem value="bookbank">Book Bank</MenuItem>
-              </Select>
-            </Grid>
-          </FormControl>
-        </div>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Accession Number</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>stream</TableCell>
-              <TableCell>Date of Submission</TableCell>
-              <TableCell>Date Pending Time</TableCell>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>stream</TableCell>
+            <TableCell>Date of Submission</TableCell>
+            <TableCell>Date Pending Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {books.map((book) => (
+            <TableRow key={book.id}>
+              <TableCell>{book.id}</TableCell>
+              <TableCell>{book.name}</TableCell>
+              <TableCell>{book.stream}</TableCell>
+              <TableCell>{book.submissionDate}</TableCell>
+              <TableCell>{book.pendingDays} days</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {books.map((book) => (
-              <TableRow key={book.id}>
-                <TableCell>{book.id}</TableCell>
-                <TableCell>{book.Accession_Number}</TableCell>
-                <TableCell>{book.name}</TableCell>
-                <TableCell>{book.stream}</TableCell>
-                <TableCell>{book.submissionDate}</TableCell>
-                <TableCell>{book.pendingDays} days</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </card>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
