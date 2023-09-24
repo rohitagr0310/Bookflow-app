748783583f902581fb5843fd8674f0a8c479a944
Jayesh Joshi <divyeshjoshi7474@gmail.com>
2023-09-24 11:32:01 +0530

    navigate_error_fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable require-jsdoc */
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/scroll/scroll-to-top.jsx";
 import HomePage from "./pages/home_page/HomePage.jsx";
 import AuthPage from "./pages/login_page/AuthPage.jsx";
@@ -24,6 +26,11 @@ import AdminDashboard from "./pages/admin_panel/adminDashboard.jsx";
 import StudentDashboard from "./pages/student_panel/studentdashboard.jsx";
 import ContactUs from "./pages/contact-us/ContactUs.jsx";
 import EmailVerifiedPage from "./pages/email_verified/EmailVerified.jsx";
+
+/*
+ * Main component for the application.
+ * @returns {JSX.Element} The rendered component.
+ */
 
 function App () {
   const theme = createTheme();
@@ -56,17 +63,14 @@ function App () {
                   <Route path="pending-issue" element={<PendingIssue />} />
                 </Route>
               )
-              : userType === "U" // Only allow access to student route if userType is "U"
-                ? (
-                  <Route path="student" element={<StudentPanel />}>
-                    <Route index element={<StudentDashboard />} />
-                    <Route path="account" element={<StudentAccount />} />
-                    <Route path="history" element={<StudentHistory />} />
-                    <Route path="search" element={<Search />} />
-                  </Route>
-                )
-                : <Navigate to="/login" /> // Redirect to login for other user types
-            }
+              : (
+                <Route path="student" element={<StudentPanel />}>
+                  <Route index element={<StudentDashboard />} />
+                  <Route path="account" element={<StudentAccount />} />
+                  <Route path="history" element={<StudentHistory />} />
+                  <Route path="search" element={<Search />} />
+                </Route>
+              )}
           </Routes>
           <Footer />
         </div>
