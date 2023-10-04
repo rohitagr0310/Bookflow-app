ca8eabb0d471fd929e092922b737e3cd63e073a2
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-10-04 09:54:57 +0530

    Lazy Loaidng added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,51 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ["eslint:recommended", "plugin:react/recommended"],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [".eslintrc.{js,cjs}"],
-			parserOptions: {
-				sourceType: "module",
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 2021, // Use the appropriate ECMAScript version
-		sourceType: "module",
-	},
-	plugins: ["react"],
-	rules: {
-		indent: ["error", 2],
-		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-		"comma-dangle": ["error", "never"],
-		"no-multiple-empty-lines": ["error", { max: 1 }], // Updated to allow only 1 blank line
-		"space-before-function-paren": ["error", "always"], // Updated to "always"
-		"no-extra-semi": "error",
-	},
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["standard", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script"
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    // Enforce consistent indentation (use tabs or spaces)
+    indent: ["error", 2],
+
+    // Enforce single quotes for string literals
+    quotes: ["error", "double"],
+
+    // Require semicolons at the end of statements
+    semi: ["error", "always"],
+
+    // Enforce consistent spacing inside curly braces
+    "object-curly-spacing": ["error", "always"],
+
+    // Enforce consistent spacing before and after arrow functions
+    "arrow-spacing": ["error", { before: true, after: true }],
+
+    // Require variable declarations to be at the top of their scope
+    "vars-on-top": "error",
+
+    // Require the use of triple equals (===) and not double equals (==)
+    eqeqeq: "error",
+
+    // Enforce a maximum line length to improve readability
+    "max-len": ["error", { code: 250 }],
+
+    // Require consistent return values in array methods like map and forEach
+    "array-callback-return": "error"
+  }
 };
