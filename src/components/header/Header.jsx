56072ba6c34967464f7bb705af3adbc0b30b40ba
Suyash Rajput <suyashrajput884@gmail.com>
2023-10-18 02:14:31 +0530

    newHeader

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./_Header.css";
 
 const Header = () => {
@@ -12,29 +12,29 @@ const Header = () => {
     <AppBar className="appBar">
       <Toolbar>
         <Typography variant="h6" className="headerText">
-          <Link to="/" className="headerLink">
+          <NavLink to="/" className="headerLink">
             BookFlow
-          </Link>
+          </NavLink>
         </Typography>
 
         <div className={isActive ? "linksWrapper mobileLinksWrapper" : "linksWrapper"}>
-          <a href="/" className="navLink" onClick={() => setIsActive(false)}>
+          <NavLink to="/" className="navLink" onClick={() => setIsActive(false)}>
             Home
-          </a>
-          <a href="/contact-us" className="navLink" onClick={() => setIsActive(false)}>
+          </NavLink>
+          <NavLink to="/contact-us" className="navLink" onClick={() => setIsActive(false)}>
             Contact
-          </a>
-          <a href="/about-us" className="navLink" onClick={() => setIsActive(false)}>
+          </NavLink>
+          <NavLink to="/about-us" className="navLink" onClick={() => setIsActive(false)}>
             Meet the Team
-          </a>
-          <a href="/terms-and-conditions" className="navLink" onClick={() => setIsActive(false)}>
+          </NavLink>
+          <NavLink to="/terms-and-conditions" className="navLink" onClick={() => setIsActive(false)}>
             Terms
-          </a>
+          </NavLink>
         </div>
 
-        <Link to="/login" className="loginLink">
+        <NavLink to="/login" className="loginLink">
           Login
-        </Link>
+        </NavLink>
         <span className="material-icons toggleNav" onClick={() => {
           setIsActive(!isActive);
         }}>
